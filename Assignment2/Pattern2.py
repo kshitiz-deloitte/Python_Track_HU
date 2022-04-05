4e65d20edb8da07aa5aebd0ce6f634580ca95d94
kshitiz-deloitte <101085691+kshitiz-deloitte@users.noreply.github.com>
2022-04-05 16:44:41 +0530

    Modified Pattern 2 and 3

--- Assignment2/Pattern2.py
+++ Assignment2/Pattern2.py
@@ -2,11 +2,11 @@ row = int(input('Enter number of rows required: '))
 
 for i in range(row):
     for j in range(row - i):
-        print(' ', end='')  # printing preceding space
+        print(' ', end=' ')  # printing preceding space
 
     for j in range(2 * i + 1):
         if j == 0 or j == 2 * i or i == row - 1:
-            print('*', end='')  # printing *
+            print('*', end=' ')  # printing *
         else:
-            print(' ', end='')  # printing space inside triangle
+            print(' ', end=' ')  # printing space inside triangle
     print()  # for new line
--- Assignment2/Pattern3.py
+++ Assignment2/Pattern3.py
@@ -1,13 +1,12 @@
 # row = int(input('Enter number of rows required: '))
-row = 3
-for i in range(row - 1, 0, -1):
-    for j in range((row - 1)-i):
-        print(i)
-        print(' ', end='')  # printing preceding space
-
+row = 5
+for i in range(row, 0, -1):
+    for j in range(row - i - 1):
+        # print(i, j)
+        print(' ', end=' ')  # printing preceding space
     for j in range(row):
-        if i == 0 or i == row-1 or j == row - 1 or i ==j:
-            print('*', end='')  # printing *
+        if i == row or i == j or j == row or j==1:
+            print('*', end=' ')  # printing *
         else:
-            print(' ', end='')  # printing space inside triangle
+            print(' ', end=' ')  # printing space inside triangle
     print()  # for new line
