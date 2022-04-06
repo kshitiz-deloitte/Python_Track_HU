fd03e2698b1e4f4552f2f0ac888ccfcc547e22ae
kshitiz-deloitte <101085691+kshitiz-deloitte@users.noreply.github.com>
2022-04-06 11:20:47 +0530

    Update in assignment 4

--- Assignment4/PythonBuiltInFunctions.py
+++ Assignment4/PythonBuiltInFunctions.py
@@ -1,7 +1,7 @@
 equation_solution = lambda a, b, c, x: print((a * x ** 2) + (b * x) + c)
 a1, b1, c1, x1 = input("Input for a b c x:").split(" ")
-equation_solution(a1, b1, c1, x1)
+equation_solution(int(a1), int(b1), int(c1), int(x1))
 
-lst1=["Alaska", "Alabama", "Arizona", "Arkansas", "Colorado", "Montana", "Nevada"]
+lst1 = ["Alaska", "Alabama", "Arizona", "Arkansas", "Colorado", "Montana", "Nevada"]
 print(list(map(lambda a: a.count('a') + a.count('A'), lst1)))
 
